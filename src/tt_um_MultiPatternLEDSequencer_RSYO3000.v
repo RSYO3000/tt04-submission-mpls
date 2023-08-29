4bb10c8690603af27d305c0d21e3a319186a8943
RSYO3000 <50602959+RSYO3000@users.noreply.github.com>
2023-08-29 01:42:12 -0600

    Update tt_um_MultiPatternLEDSequencer_RSYO3000.v

--- src/tt_um_MultiPatternLEDSequencer_RSYO3000.v
+++ src/tt_um_MultiPatternLEDSequencer_RSYO3000.v
@@ -1,10 +1,10 @@
 module tt_um_MultiPatternLEDSequencer_RSYO3000 (
-    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
-    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches for clk_selector and pattern_sel
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 LEDs
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
     output wire [7:0] uio_out,  // IOs: Bidirectional Output path
     output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
+    input  wire       ena,      // will go high when the design is enabled, not used circuit can be turned off when pattern_sel = 0
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
